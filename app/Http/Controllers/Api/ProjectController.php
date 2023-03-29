2d61f537486c4dee16631da683dace7091970ab3
FontaMatte <matteo.fontanel@gmail.com>
2023-03-29 15:59:26 +0200

    Milestone 1 Completed

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+// Models
+use App\Models\Project;
+
+class ProjectController extends Controller
+{
+    public function index() 
+    {
+        $projects = Project::all();
+        
+        return response()->json([
+            'success' => true,
+            'code' => 200,
+            'message' => 'Ok',
+            'projects' => $projects
+        ]);
+    }
+}
